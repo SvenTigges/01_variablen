ef6b9d380c1f6c549a0a56fe048bae1d7643b3cf
SvenTigges <sven.tigges@web.de>
2021-05-18 11:34:19 +0200

    Erster Teil Aufrag

--- script_01.js
+++ script_01.js
@@ -1,1 +1,22 @@
 /***** Variablen 01 *******/
+
+// Zeilen-Kommentar
+
+/**
+   Block-Kommentar
+ */
+
+//Ausgabe
+//console.log("Hello World");
+//console.log(firstName);
+
+// Deklaration + Wertzuweisung I
+/**/
+let firstName; // Deklaration (Definition)
+firstName = "Max" // Wertzuweisung 
+console.log(firstName); // Ausgabe 
+let familyName = "Muster"; // Deklaration + Wertzuweisung (14+15 zusammengefast)
+console.log(familyName); // Ausgabe
+console.log(firstName + " "  + familyName); // leerzeichen und ausgaben kombiniert 
+
+
