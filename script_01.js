afa2c981fd29fda9f1db04bac4c4c346065f379e
Peter <schmidt.multimedia@gmail.com>
2021-05-18 11:35:14 +0200

    Deklaration + Wertzuweisung I

--- script_01.js
+++ script_01.js
@@ -1,1 +1,21 @@
 /***** Variablen 01 *******/
+
+// Zeilen-Kommentar
+
+/**
+    Block-Kommentar
+ */
+
+// Ausgabe
+//console.log("Hallo Welt!");
+//console.log(firstName);
+
+// Deklaration + Wertzuweisung I 
+/**/
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log(familyName); // Ausgabe
+console.log(firstName + " " + familyName);
+
