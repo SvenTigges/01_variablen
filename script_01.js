78a11ab6b1f63529389bef6390b4271d87fe8ab3
SvenTigges <sven.tigges@web.de>
2021-05-18 11:49:27 +0200

    Deklaration + Wertzuweisung II

--- script_01.js
+++ script_01.js
@@ -3,7 +3,6 @@
 // Zeilen-Kommentar
 
 /**
-<<<<<<< HEAD
    Block-Kommentar
  */
 
@@ -12,29 +11,17 @@
 //console.log(firstName);
 
 // Deklaration + Wertzuweisung I
-/**/
+/*
 let firstName; // Deklaration (Definition)
 firstName = "Max" // Wertzuweisung 
 console.log(firstName); // Ausgabe 
 let familyName = "Muster"; // Deklaration + Wertzuweisung (14+15 zusammengefast)
 console.log(familyName); // Ausgabe
 console.log(firstName + " "  + familyName); // leerzeichen und ausgaben kombiniert 
+*/
 
-=======
-    Block-Kommentar
- */
-
-// Ausgabe
-//console.log("Hallo Welt!");
-//console.log(firstName);
-
-// Deklaration + Wertzuweisung I 
-/**/
-let firstName; // Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung
-console.log(firstName); // Ausgabe
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log(familyName); // Ausgabe
-console.log(firstName + " " + familyName);
->>>>>>> afa2c981fd29fda9f1db04bac4c4c346065f379e
-
+// Deklaration + Wertzuweisung II
+let firstName,familyName;
+firstName = "Test";
+familyName = "Test";
+console.log("Hallo, " + firstName + " "  + familyName +"!");
